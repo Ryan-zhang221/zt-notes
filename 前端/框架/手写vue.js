dbc37d93e1603e062a7dc699ee61f18f9901acb2
ryan <>
2022-11-13 10:42:50 +0800

    手写vue computed

--- 前端/框架/手写vue.js
+++ 前端/框架/手写vue.js
@@ -15,23 +15,75 @@
 const effectPool = []
 let currentEffect = null
 
+/*
+	响应式对照表，每个属性对应一个数组，数组里面是对应要更新的
+	effect函数，或者是多个组件（组件也就是effect函数）
+	const proxyTable = {
+		obj: {
+			key: [effect1, effect2]
+		}
+	}
+	我希望对象中 key 是任意的东西，但是对象的话，key 只能是数字、字符串
+	symbol这三种类型，所以我们使用 Map 做存储，所以要优化下
+	key => new Set() why？不会用重复值的数组，强化的数组
+	obj => new Map() why？key可以是任意数据类型
+	proxyTable => new weakMap() why? weakMap中存的key value是弱引用
+	其中的 key 可以被垃圾回收，否则 Map 的话不会垃圾回收，就会造成内存泄漏
+*/
+const proxyDeps = new WeakMap()
+
 function reactive(value) {
 	return new Proxy(value, {
 		get(target, key, receiver) {
-			console.log(2)
-			if(currentEffect) {
-				effectPool.push(currentEffect)
+			// 改造前
+			// if(currentEffect) {
+			// 	effectPool.push(currentEffect)
+			// }
+			if (currentEffect) {
+				let deps = proxyDeps.get(target)
+				if(!deps) {
+					deps = new Map()
+					proxyDeps.set(target, deps)
+				}
+				let dep = deps.get(key)
+				if(!dep) {
+					dep = new Set()
+					deps.set(key, dep)
+				}
+				dep.add(currentEffect)
 			}
-			return Reflect.get(...arguments)
+
+			const res = Reflect.get(...arguments)
+			// return res
+			return typeof res === "object" ? reactive(res) : res
 		},
 		set(target, key, receiver) {
-			console.log(3)
+			// 改造前
 			// 这里必须先让值更新，最后再去执行逻辑
-			try {
-				return Reflect.set(...arguments)
-			} finally { // 这里 finally 的作用是在 return 之后还可以写逻辑
-				effectPool.forEach(fn => fn())
+			// try {
+			// 	return Reflect.set(...arguments)
+			// } finally { // 这里 finally 的作用是在 return 之后还可以写逻辑
+			// 	effectPool.forEach(fn => fn())
+			// }
+			const res = Reflect.set(...arguments)
+			const deps = proxyDeps.get(target)
+			if(deps) {
+				const dep = deps.get(key)
+				if(dep) {
+					dep.forEach(fn => {
+						if(fn.computed) {
+							fn.dirty = true
+						}
+					})
+					dep.forEach(fn => {
+						if(!fn.computed) {
+							fn()
+						}
+					})
+				}
 			}
+
+			return res
 		}
 	})
 }
@@ -67,35 +119,138 @@ function ref(value) {
 	}
 }
 
-function effect(fn) {
-	console.log(1)
-	currentEffect = fn
-	fn()
-	currentEffect = null
+/*
+	effect 函数收集还有问题，如果发生了函数嵌套，那么就会出现
+	依赖收集不到的情况。vue是一个精确收集的过程，目前的 effect
+	函数做不到这一点。现在是reactive函数中任何嵌套的变量改变都
+	会触发更新，我们想做到精确更新。
+	之前的：
+	function effect(fn) {
+		currentEffect = fn
+		fn()
+		currentEffect = null
+	}
+*/
+function effect(fn, options = {}) {
+	/* options的结构如下
+		options: {
+			lazy: false
+		}
+	*/
+	// 创建一个包装后的函数effectFn，往它身上挂一些东西进去
+	const effectFn = Object.assign(function() {
+		effectFn.parent = currentEffect
+		currentEffect = effectFn
+		const res = fn()
+		currentEffect = effectFn.parent
+		currentEffect = null
+		return res
+	}, {
+		parent: null,
+		dirty: true,
+		computed: false
+	}, options)
+
+	if (!options.lazy) {
+		effectFn()
+	}
+
+	return effectFn
+} 
+
+
+
+
+
+// 实现 computed (缓存/脏值检测)
+/* 改造前
+	function computed(getter) {
+		let value = void 0 // undefined
+		let dirty = true
+
+		effect(() => {
+			value = getter()
+		})
+
+		return {
+			get value() {
+				if (currentEffect) {
+					effectPool.push(currentEffect)
+				}
+				return dirty ? getter() : value
+			},
+			set value(newValue) {
+				console.log('computed没有传递修改函数')
+			}
+		}
+	}
+*/
+function computed(getter, setter) {
+	let _value = getter()
+
+	return new (class {
+		get value() {
+			if(this._effect.dirty) {
+				this._effect()
+			}
+			return _value
+		}
+		set value(newValue) {
+			if(setter) {
+				effect(newValue)
+			} else {
+				console.log('setter没有，改不了')
+			}
+		}
+
+		constructor() {
+			const self = this
+			this._effect = effect(() => {
+				self.dirty = false
+				_value = getter()
+			}, {lazy: true, computed: true})
+		}
+	})()
 }
+
+
+
 
 // ----------------------------------------------
 // 此时 obj 就变成了一个代理后的对象
-const obj = reactive({ num: 1 })
+// const obj = reactive({ num: 1 })
 
 // 这里可以类比于 template 中内容转化为的函数，数据改变后每次更新
 // 视图 = template 编译后的渲染函数
-effect(function() {
-	// 这里获取了 obj.name 触发了 get 方法
-	// obj.num + 10
-	let b = obj.num
-})
+// effect(function() {
+// 	// 这里获取了 obj.name 触发了 get 方法
+// 	// obj.num + 10
+// 	let b = obj.num
+// })
 
 // 这里改变 obj.num 就会触发 set 方法
-obj.num += 1
+// obj.num += 1
 
 // ----------------------------------------------
 // ref
-let refValue = ref(1)
-effect(function() {
-	let c = refValue + 2
+// let refValue = ref(1)
+// effect(function() {
+// 	let c = refValue + 2
+// })
+// refValue += 8
+
+// ----------------------改造后的精确更新------------------------
+const obj1 = reactive({
+	num: 1,
+	child: {
+		num: 100
+	}
 })
-refValue += 8
+effect(() => {
+	console.log('我更新了', obj1.child.num)
+})
+// obj1.num += 1
+obj1.child.num += 1
 
 
 
